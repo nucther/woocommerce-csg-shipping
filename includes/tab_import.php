b2e83d033d89e900a7e9d6b1e53dbff0de28b4af
nurohman <me@nurohman.com>
2018-02-07 19:46:04 +0700

    Changing import csv with selection type

--- includes/tab_import.php
+++ includes/tab_import.php
@@ -19,12 +19,61 @@ if( isset($_POST['upload_csv'])){
     } else{
         print_r($movefile);
     }
+
+    $type = $_POST['data_type'];
+    switch($type){
+        case 'cz_fedex':
+            $type = 'country zone';
+            $provider = 'FedEx';
+            $package = 'default';
+            break;
+        case 'cz_dhl': 
+            $type = 'country zone';
+            $provider = 'DHL';
+            $package = 'default';
+            break;
+        case 'cz_tnt':
+            $type = 'country zone';
+            $provider = 'TNT';
+            $package = 'default';
+            break;
+        case 'rate_fedex': 
+            $type = 'shipping rate';
+            $provider = 'FedEx';
+            $package = 'default';
+            break;
+        case 'rate_dhl':
+            $type = 'shipping rate'; 
+            $provider = 'DHL';
+            $package = 'default';
+            break;
+        case 'rate_tnt':
+            $type = 'shipping rate';
+            $provider = 'TNT';
+            $package = 'default';
+            break;
+    }
 }
 
  ?>
  <div class="table-box" style="border: 1px solid #ccc; border-top: none;padding: 20px;">    
  <table class="form-table wc-shipping-zone-settings">
             <tbody>
+                <tr>
+                    <th class="titledesc">
+                        Select database to updated
+                    </th>
+                    <td class="forminp">
+                        <select name="data_type">
+                            <option value="cz_fedex">Country Zone FedEx</option>
+                            <option value="cz_dhl">Country Zone DHL</option>
+                            <option value="cz_tnt">Country Zone TNT</option>
+                            <option value="rate_fedex">Shipping Rate FedEx</option>
+                            <option value="rate_dhl">Shipping Rate DHL</option>
+                            <option value="rate_tnt">Shipping Rate TNT</option>
+                        </select>
+                    </td>
+                </tr>
                 <tr>
                     <th class="titledesc">
                         Update Database
@@ -42,90 +91,78 @@ if( isset($_POST['upload_csv'])){
                 </tr>
                 <tr>
                     <td>
-                    <?php 
-                        if(! is_null($csv)){
-                            if( ($handle = fopen($csv,'r') ) !== FALSE){
-                                $type = '';
-                                $provider = '';
-                                $package = '';                                
-                                $clean = false;
-                                $exception = array('type','name provider','package','weight','country name');
-                                $num = 0;
-                                while( ($data = fgetcsv($handle, 1000, ',')) !== FALSE){                                    
-                                    switch(strtolower($data[0])){
-                                        case 'type': 
-                                            $type = $data[1];
-                                            echo 'Detected : '. $type .'<br>';
-                                            $clean = true;
-                                            $num = 0;
-                                            break;
-                                        case 'name provider':
-                                            $provider= $data[1];
-                                            echo 'Provider: '. $provider .'<br>';
-                                            break;
-                                        case 'package': 
-                                            $package = $data[1];
-                                            $calc->add_package($provider, $package);
-                                            echo 'Package: '. $package .'<br>';
-                                            break;
-                                        case 'weight':                                             
-                                            $zone = [];
-                                            foreach($data as $z){
-                                                $zone[] = preg_replace('/zone\s?/i','', $z);
-                                            }                                            
-                                            break;
-                                    }
+                        <?php 
+                            if(! is_null($csv)){
+                                if( ($handle = fopen($csv,'r')) !== FALSE ){
+                                    $exception = array('type','name provider','package','weight','country name');
+                                    $num = 0;
+                                    $clean = true;
 
-                                    if($clean){
-                                        if( strtolower($type) == 'country zone' && !empty($provider)){
-                                            $calc->clean_country($provider);
-                                            $clean = false;
-                                        }
-                                        
-                                        if( strtolower($type)=='shipping rate' && !empty($provider) && !empty($package) ){
-                                            $calc->clean_rate($provider, $package);
-                                            $clean = false;
-                                        }                                        
-                                    }
+                                    while( ($data = fgetcsv($handle, 1000, ',') ) !== FALSE ){
+                                        $head =  strtolower( $data[0] );
+                                        switch( $head ){
+                                            case 'package':
+                                                $package = $data[1];
+                                                $calc->add_package($provider, $package);
+                                                break;
 
-                                    if( strtolower($type) == 'country zone'){
-                                        $cv=''; $ct='';
-                                        if(isset($data[2]) && !empty($data[2])){
-                                            $ct = 'postcode';
-                                            $cv = $data[2];
+                                            case 'weight':                                             
+                                                $zone = [];
+                                                foreach($data as $z){
+                                                    $zone[] = preg_replace('/zone\s?/i','', $z);
+                                                }                                            
+                                                break;
                                         }
-                                        
-                                        if( isset($data[3]) && !empty($data[3])){
-                                            $ct = 'city';
-                                            $cv = $data[3];
-                                        }
-                                        
-                                        if( !empty($data[0]) && !in_array(strtolower($data[0]), $exception ) ){
-                                            $calc->add_country($provider,$data[1], $data[0],$ct, $cv, $data[4] );
-                                            $num++;$ct='';
-                                        }
-                                    }
 
-                                    if( strtolower($type) == 'shipping rate'){
-                                        $condition = '';
-                                        foreach($data as $n => $value){
-                                            if(strtolower($zone[$n]) == 'weight'){
-                                                $condition = $data[0];
-                                            }else{
-                                                if( !empty($data[0]) && !in_array(strtolower($data[0]), $exception ) ){
-                                                    $calc->add_rate($provider, $package, $condition, $zone[$n], $value);
+                                        if($clean){
+                                            if( strtolower($type) == 'country zone' && !empty($provider)){
+                                                $calc->clean_country($provider);
+                                                $clean = false;
+                                            }
+                                            
+                                            if( strtolower($type)=='shipping rate' && !empty($provider) && !empty($package) ){
+                                                $calc->clean_rate($provider, $package);
+                                                $clean = false;
+                                            }
+                                        }
+
+                                        if( strtolower($type) == 'country zone'){
+                                            $cv=''; $ct='';
+                                            if(isset($data[2]) && !empty($data[2])){
+                                                $ct = 'postcode';
+                                                $cv = $data[2];
+                                            }
+                                            
+                                            if( isset($data[3]) && !empty($data[3])){
+                                                $ct = 'city';
+                                                $cv = $data[3];
+                                            }
+                                            
+                                            if( !empty($data[0]) && !in_array(strtolower($data[0]), $exception ) ){
+                                                $calc->add_country($provider,$data[1], $data[0],$ct, $cv, $data[4] );
+                                                $num++;$ct='';
+                                            }
+                                        }
+
+                                        if( strtolower($type) == 'shipping rate'){
+                                            $condition = '';
+                                            foreach($data as $n => $value){
+                                                if(strtolower($zone[$n]) == 'weight'){
+                                                    $condition = $data[0];
+                                                }else{
+                                                    if( !empty($data[0]) && !in_array(strtolower($data[0]), $exception ) ){
+                                                        $calc->add_rate($provider, $package, $condition, $zone[$n], $value);
+                                                    }
                                                 }
                                             }
                                         }
-                                    }
 
+                                    }
                                 }
+                                fclose($handle);                            
+                                unlink($csv);
                             }
-                            fclose($handle);
-                            
-                            unlink($csv);
-                        }
-                    ?>
+                        ?>
                     </td>
                 </tr>
             </tbody>
